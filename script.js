d1f64afd5a262958da015d45e453a45311fdf7c9
Riccardo Foti <riccardofoti97@gmail.com>
2023-03-08 17:05:22 +0000

    declare playRound function

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log('hello world');
-
-// create getComputerChoice function
+// declare getComputerChoice function
   // returns random item between rock, paper, scissors
 
 function getComputerChoice(){
@@ -8,4 +6,57 @@ function getComputerChoice(){
   let randomItem = choices[Math.floor(Math.random()*choices.length)];
   return randomItem;
 }
-console.log(getComputerChoice());
+
+// declare playRound function that takes two parameters (playerSelection, computerSelection)
+    // returns a string that declares the winner of the round
+    // if playerSelection === 'Rock' & computerSelection === 'Scissors'
+      // you win
+    // elif playerSelection === 'Paper' & computerSelection === 'Rock'
+      // you win
+    // elif playerSelection === 'Scissors' & computerSelection === 'Paper'
+      // you win
+    // elif playerSelection === 'Rock' & computerSelection === 'Rock'
+      // tie
+    // elif playerSelection === 'Paper' & computerSelection === 'Paper'
+      // tie
+    // elif playerSelection === 'Scissors & computerSelection === 'Scissors'
+      // tie
+    //else
+      // you lose
+
+function playRound(playerSelection, computerSelection) {
+  let winnerDeclaration = `You Win! ${playerSelection} beats ${computerSelection}`;
+  let loserDeclaration = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  let tieDeclaration = "It's a tie!";
+
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+    return winnerDeclaration;
+  }
+  else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+    return winnerDeclaration;
+  }
+  else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+    return winnerDeclaration;
+  }
+  else if (playerSelection === 'Rock' && computerSelection === 'Rock'){
+    return tieDeclaration;
+  }
+  else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
+    return tieDeclaration;
+  }
+  else if (playerSelection === 'Scissors' && computerSelection === 'Scissors'){
+    return tieDeclaration;
+  }
+  else {
+    return loserDeclaration;
+  }
+}
+
+
+const playerSelection = 'Scissors';
+const computerSelection = getComputerChoice();
+
+console.log(`Player Selection = ${playerSelection}`);
+console.log(`Computer Selection = ${computerSelection}`);
+
+console.log(playRound(playerSelection,computerSelection));
